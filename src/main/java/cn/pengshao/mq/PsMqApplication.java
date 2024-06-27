39417af3328e023ab39e55540824b5b6773e28ed
yezp <yezhipengkobe@163.com>
2024-06-27 22:51:41 +0800

    init mq application

---
+++ src/main/java/cn/pengshao/mq/PsMqApplication.java
@@ -0,0 +1,18 @@
+package cn.pengshao.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * mq
+ *
+ * @Author: yezp
+ * @date 2024/6/27 22:48
+ */
+@SpringBootApplication
+public class PsMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsMqApplication.class, args);
+    }
+}
